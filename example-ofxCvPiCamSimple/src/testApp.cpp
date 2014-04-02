54f76b65c9bd2039740f5497493e2dffe6bc8424
orgicus <george@hirschandmann.com>
2014-04-02 11:03:10 +0100

    added example

---
+++ example-ofxCvPiCamSimple/src/testApp.cpp
@@ -0,0 +1,82 @@
+#include "testApp.h"
+#include <opencv2/opencv.hpp>
+
+using namespace ofxCv;
+using namespace cv;
+
+Mat frame,frameProcessed;
+
+void testApp::onCharacterReceived(SSHKeyListenerEventData& e)
+{
+	keyPressed((int)e.character);
+}
+//--------------------------------------------------------------
+void testApp::setup(){
+	consoleListener.setup(this);
+	consoleListener.startThread(false, false);
+
+    cam.setup(320,240,false);//setup camera (w,h,color = true,gray = false);
+    thresh = 127;
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+    frame = cam.grab();
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+    if(!frame.empty()) {
+        threshold(frame,frameProcessed,thresh,255,0);
+        drawMat(frame,0,0);
+        drawMat(frameProcessed,320,0);
+    }
+    ofDrawBitmapString("threshold: " + ofToString(thresh),320,10);
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed  (int key){
+	ofLogVerbose() << "keyPressed: " << key;
+    if(key == 't' && thresh > 0)   thresh--;
+    if(key == 'T' && thresh < 255) thresh++;
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+    thresh = ofMap(x,0,ofGetWidth(),0,255);
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
